3fd8a8967e7504ca51bcf2cf85e6ed9dc7b81f64
Radosław Sobczak <r.sobczak@onet.eu>
2022-04-23 22:24:13 +0200

    Initial Commit

---
+++ BloodPressureMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BloodPressureMonitor
+//
+//  Created by Radoslaw Sobczak on 23/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
